dcf7fc2013a9c65ec86dd686d0975f6ae7bcde00
Oshan Shrestha <oshan_shrestha@outlook.com>
2019-12-15 14:35:44 +0545

    getData function slightly modified

--- routes/data.js
+++ routes/data.js
@@ -13,9 +13,20 @@ router.post('/getData', async (req, res) => {
 
         if (user) {
             if (user.token.expiration >= Date.now()) {
-                let facSem = user.faculty + user.currentYear
+                let facSem = ""
+                facSem = (user.faculty + user.currentYear).toString();
                 console.log("facSem: ", facSem)
-                let routineAll = await Routine.findOne({ facSem })
+                // let routine = new Routine({
+                //     weekDay: 2,
+                //     time: 900,
+                //     subject: "Microelectronics",
+                //     sCode: "EEEG211",
+                //     lecturer: "Santosh Parajuli",
+                //     classroom: "9 404",
+                //     facSem
+                // })
+                // await routine.save()
+                let routineAll = await Routine.find({ sCode: "EEEG211" })
                 res.status(200).json({ routineAll })
             } else {
                 res.status(501).json({
